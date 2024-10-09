456a9ee3a98ebbb7ad9cc008993885cfefeafa39
Satyaki Pal <satyakipal111@gmail.com>
2024-10-10 00:21:37 +0530

    landing css

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
+import '../styles/Landing.css'
 
 const Landing = () => {
     return ( 
         <div className="landing">
             <Link to='/adminlogin'>
+                <img src="../admin_logo.png" alt="" />
                 <h1>Admin</h1>
             </Link>
             <Link to='/userlogin'>
+                <img src="../user_logo.png" alt="" />
                 <h1>User</h1>
             </Link>
         </div>
