49d0ae7562d0bc04317e163b488e5cb5f60d2b35
Satyaki Pal <satyakipal111@gmail.com>
2024-10-09 09:48:24 +0530

    adminlogin login component

--- src/Components/AdminLogin.jsx
+++ src/Components/AdminLogin.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 export default function AdminLogin() {
+  let [username,setUsername] = useState("");
+  let [password,setPassword] = useState("");
+  console.log(username);
+  console.log(password);
+  function Login(){
+    if(username === "Satyaki" && password === "12345"){
+      alert("Admin logged in");
+    }else{
+      alert("Invalid credentials");
+    }
+  }
   return (
-    <div>AdminLogin</div>
+    <div className="AdminLogin">
+      <form action="">
+        <label>Username: </label>
+        <input type="text" value={username} onChange={(r)=>{setUsername(r.target.value)}} placeholder="Enter the username" required />
+        <label>Password: </label>
+        <input type="text" value={password} onChange={(r)=>{setPassword(r.target.value)}} placeholder="Enter the password" required />
+        <button onClick={Login}>Login</button>
+      </form>
+    </div>
   )
 }
