63b709c082d1b33613f645f0733b6b3a92560307
Satyaki Pal <satyakipal111@gmail.com>
2024-10-09 20:48:06 +0530

    adminlogin css update

--- src/Components/AdminLogin.jsx
+++ src/Components/AdminLogin.jsx
@@ -14,13 +14,15 @@ export default function AdminLogin() {
   }
   return (
     <div className="AdminLogin">
-      <form action="">
-        <label>Username: </label>
-        <input type="text" value={username} onChange={(r)=>{setUsername(r.target.value)}} placeholder="Enter the username" required />
-        <label>Password: </label>
-        <input type="text" value={password} onChange={(r)=>{setPassword(r.target.value)}} placeholder="Enter the password" required />
-        <button onClick={Login}>Login</button>
-      </form>
+      <div className="form_container">
+        <form action="">
+          <label>Username: </label>
+          <input type="text" value={username} onChange={(r)=>{setUsername(r.target.value)}} placeholder="Enter the username" required />
+          <label>Password: </label>
+          <input type="text" value={password} onChange={(r)=>{setPassword(r.target.value)}} placeholder="Enter the password" required />
+          <button onClick={Login}>Login</button>
+        </form>
+      </div>
     </div>
   )
 }
