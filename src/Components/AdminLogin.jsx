a776ed86e3701753366ced6410d4298ae309f70e
Satyaki Pal <satyakipal111@gmail.com>
2024-10-19 11:21:43 +0530

    bootstrap modifications

--- src/Components/AdminLogin.jsx
+++ src/Components/AdminLogin.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/AdminLogin.css'
+import React, { useEffect, useState } from 'react';
+import '../styles/AdminLogin.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 export default function AdminLogin() {
-  let [username,setUsername] = useState("");
-  let [password,setPassword] = useState("");
+  let [username, setUsername] = useState('');
+  let [password, setPassword] = useState('');
   console.log(username);
   console.log(password);
 
-  let [admin,setAdmin] = useState([]);
-  useEffect(()=>{
-    async function fetchAdmin(){
+  let [admin, setAdmin] = useState([]);
+  useEffect(() => {
+    async function fetchAdmin() {
       let data = await fetch('http://localhost:8000/Admin');
       let res = await data.json();
       setAdmin(res);
     }
     fetchAdmin();
-  },[]);
+  }, []);
 
   // function Login(){
   //   let count = admin.reduce((count,i)=>{
@@ -26,33 +28,70 @@ export default function AdminLogin() {
   //   }
   // };
 
-  function Login(){
-    let val = admin.filter((x)=>{
-      return x.username===username && x.password===password;
+  function Login() {
+    let val = admin.filter((x) => {
+      return x.username === username && x.password === password;
     });
-    if(val.length>0){
+    if (val.length > 0) {
       alert('Login Successful');
-    }else{
+    } else {
       alert('Login Failed');
     }
   }
 
   return (
+    // <div className="AdminLogin">
+    //   <div className="form_container">
+    //     <div>
+    //       <img src="../admin_logo.png" alt="" />
+    //       <h2>Admin Login</h2>
+    //     </div>
+    //     <form action="">
+    //       <label>Username: </label>
+    //       {/* r.target.value = r is the event and terget is used to target the element on which event in happening and value is uded to get the value */}
+    //       <input type="text" value={username} onChange={(r)=>{setUsername(r.target.value)}} placeholder="Enter the username" required />
+    //       <label>Password: </label>
+    //       <input type="text" value={password} onChange={(r)=>{setPassword(r.target.value)}} placeholder="Enter the password" required />
+    //       <button onClick={Login}>Login</button>
+    //     </form>
+    //   </div>
+    // </div>
+
     <div className="AdminLogin">
-      <div className="form_container">
-        <div>
+      <div>
+        <div class="head">
           <img src="../admin_logo.png" alt="" />
           <h2>Admin Login</h2>
         </div>
-        <form action="">
-          <label>Username: </label>
-          {/* r.target.value = r is the event and terget is used to target the element on which event in happening and value is uded to get the value */}
-          <input type="text" value={username} onChange={(r)=>{setUsername(r.target.value)}} placeholder="Enter the username" required />
-          <label>Password: </label>
-          <input type="text" value={password} onChange={(r)=>{setPassword(r.target.value)}} placeholder="Enter the password" required />
-          <button onClick={Login}>Login</button>
-        </form>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={username}
+              onChange={(r) => {
+                setUsername(r.target.value);
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(r) => {
+                setPassword(r.target.value);
+              }}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={Login}>
+            Submit
+          </Button>
+        </Form>
       </div>
     </div>
-  )
+  );
 }
